341479108b2ad0b36c89c7a8749b5222e6d8ed31
GregoryBiter <gregorybiter@gmail.com>
2026-03-14 19:10:13 +0000

    delete file e-commerse

--- upload/admin/controller/startup/application.php
+++ upload/admin/controller/startup/application.php
@@ -19,29 +19,29 @@ class Application extends \Opencart\System\Engine\Controller {
 		$this->registry->set('customer', new \Opencart\System\Library\Cart\Customer($this->registry));
 
 		// Currency
-		$this->registry->set('currency', new \Opencart\System\Library\Cart\Currency($this->registry));
+		// $this->registry->set('currency', new \Opencart\System\Library\Cart\Currency($this->registry));
 
 		// Tax
-		$this->registry->set('tax', new \Opencart\System\Library\Cart\Tax($this->registry));
+		// $this->registry->set('tax', new \Opencart\System\Library\Cart\Tax($this->registry));
 
-		if ($this->config->get('config_tax_default') == 'shipping') {
-			$this->tax->setShippingAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
-		}
+		// if ($this->config->get('config_tax_default') == 'shipping') {
+		// 	$this->tax->setShippingAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+		// }
 
-		if ($this->config->get('config_tax_default') == 'payment') {
-			$this->tax->setPaymentAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
-		}
+		// if ($this->config->get('config_tax_default') == 'payment') {
+		// 	$this->tax->setPaymentAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+		// }
 
-		$this->tax->setStoreAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
+		// $this->tax->setStoreAddress((int)$this->config->get('config_country_id'), (int)$this->config->get('config_zone_id'));
 
 		// Weight
-		$this->registry->set('weight', new \Opencart\System\Library\Cart\Weight($this->registry));
+		// $this->registry->set('weight', new \Opencart\System\Library\Cart\Weight($this->registry));
 
 		// Length
-		$this->registry->set('length', new \Opencart\System\Library\Cart\Length($this->registry));
+		// $this->registry->set('length', new \Opencart\System\Library\Cart\Length($this->registry));
 
 		// Cart
-		$this->registry->set('cart', new \Opencart\System\Library\Cart\Cart($this->registry));
+		// $this->registry->set('cart', new \Opencart\System\Library\Cart\Cart($this->registry));
 
 		$this->load->helper('validation');
 	}
--- upload/system/storage/vendor/composer/autoload_psr4.php
+++ upload/system/storage/vendor/composer/autoload_psr4.php
@@ -11,12 +11,4 @@ return array(
     'Symfony\\Polyfill\\Php81\\' => array($vendorDir . '/symfony/polyfill-php81'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
-    'ScssPhp\\ScssPhp\\' => array($vendorDir . '/scssphp/scssphp/src'),
-    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
-    'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
-    'JmesPath\\' => array($vendorDir . '/mtdowling/jmespath.php/src'),
-    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
-    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
-    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
-    'Aws\\' => array($vendorDir . '/aws/aws-sdk-php/src'),
 );
